2eceaee34ef212a35d089dcabca9556de15d8f69
matthew-shaw <matthew.shaw@digital.landregistry.gov.uk>
2016-07-15 23:04:31 +0100

    set locale

--- script/provision.sh
+++ script/provision.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+echo 'Setting locale...'
+sudo locale-gen en_GB.UTF-8
+
 echo 'Installing PostgreSQL & PostGIS...'
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 sudo apt-get install wget ca-certificates
@@ -7,7 +10,6 @@ wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get install -y postgresql-9.5-postgis-2.2 pgadmin3 postgresql-contrib postgis libpq-dev python-dev
-sudo locale-gen en_GB.UTF-8
 
 echo 'Installing Pip...'
 apt-get -y install python-pip
