0980ac5693fc26687a73e4a04e87aedd58d2dd22
matthew-shaw <matthew.shaw@digital.landregistry.gov.uk>
2016-10-03 13:49:03 +0100

    set mimetype for json responses

--- application/views.py
+++ application/views.py
@@ -1,3 +1,4 @@
+from flask import Response
 from application import app
 from application.models import Constituency
 
@@ -5,10 +6,10 @@ from application.models import Constituency
 @app.route('/constituencies', methods=["GET"])
 def constituencies():
     constituencies = Constituency.query.all()
-    return repr(constituencies)
+    return Response(repr(constituencies), mimetype='application/json')
 
 
 @app.route('/constituencies/<id>', methods=["GET"])
 def constituency(id):
     constituency = Constituency.query.get(id)
-    return repr(constituency)
+    return Response(repr(constituency), mimetype='application/json')
