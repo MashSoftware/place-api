64a35d1d3d0861f391d8e385adc4ad6176304ac7
matthew-shaw <matthew.shaw@digital.landregistry.gov.uk>
2016-10-07 00:22:54 +0100

    response caching

--- mash_geo_api/views/constituencies.py
+++ mash_geo_api/views/constituencies.py
@@ -1,16 +1,21 @@
-from flask import Response, Blueprint
+from flask import Response, Blueprint, request
+from mash_geo_api import app, cache
 from mash_geo_api.models import Constituency
 
 constituencies_bp = Blueprint('constituencies', __name__)
 
 
 @constituencies_bp.route('/', methods=["GET"])
+@cache.cached(timeout=86400)
 def constituencies():
+    app.logger.info('GET ' + request.url)
     constituencies = Constituency.query.all()
     return Response(repr(constituencies), mimetype='application/json')
 
 
 @constituencies_bp.route('/<id>', methods=["GET"])
+@cache.memoize(timeout=86400)
 def constituency(id):
+    app.logger.info('GET ' + request.url)
     constituency = Constituency.query.get(id)
     return Response(repr(constituency), mimetype='application/json')
