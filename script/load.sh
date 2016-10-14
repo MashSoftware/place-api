54aef24fc3ac03e2a4bd3e80d9a37f114de778ce
matthew-shaw <matthew.shaw@digital.landregistry.gov.uk>
2016-10-14 14:34:47 +0100

    pg uri on alter commands to improve remote running

--- script/load.sh
+++ script/load.sh
@@ -3,8 +3,8 @@
 for f in /vagrant/data/*.shp
 do
   echo "--- LOADING SHAPEFILE: $filename ---"
-  shp2pgsql -d -I-t 2D -s 27700:4326 $f public.constituency | psql -q $DATABASE_URL
-  psql -c "ALTER TABLE public.constituency DROP CONSTRAINT constituency_pkey;"
-  psql -c "ALTER TABLE public.constituency ADD PRIMARY KEY (code);"
+  shp2pgsql -d -I -s 27700:4326 $f public.constituency | psql -q $DATABASE_URL
+  psql $DATABASE_URL -c "ALTER TABLE public.constituency DROP CONSTRAINT constituency_pkey;"
+  psql $DATABASE_URL -c "ALTER TABLE public.constituency ADD PRIMARY KEY (code);"
   echo "--- COMPLETED LOADING SHAPEFILE: $filename ---"
 done
