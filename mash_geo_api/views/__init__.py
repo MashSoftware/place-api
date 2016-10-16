8d46fb592bea9116931374c5f35810bab47e740e
matthew-shaw <matthew.shaw@digital.landregistry.gov.uk>
2016-10-16 20:44:29 +0100

    http 400 series error handling

---
+++ mash_geo_api/views/__init__.py
@@ -0,0 +1,19 @@
+from mash_geo_api import app
+from flask import Response
+import json
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return Response(response=json.dumps({"status_code": "404",
+                                         "message": "Not Found"}),
+                    mimetype='application/json',
+                    status=404)
+
+
+@app.errorhandler(405)
+def method_not_allowed(error):
+    return Response(response=json.dumps({"status_code": "405",
+                                         "message": "Method Not Allowed"}),
+                    mimetype='application/json',
+                    status=405)
--- mash_geo_api/views/constituencies.py
+++ mash_geo_api/views/constituencies.py
@@ -5,15 +5,17 @@ from mash_geo_api.models import Constituency
 constituencies_bp = Blueprint('constituencies', __name__)
 
 
-@constituencies_bp.route('/', methods=["GET"])
+@constituencies_bp.route('/', methods=['GET'])
 @cache.cached(timeout=86400)
 def constituencies():
     constituencies = Constituency.query.all()
-    return Response(repr(constituencies), mimetype='application/json', status=200)
+    return Response(repr(constituencies),
+                    mimetype='application/json',
+                    status=200)
 
 
-@constituencies_bp.route('/<ons_code>', methods=["GET"])
+@constituencies_bp.route('/<ons_code>', methods=['GET'])
 @cache.memoize(timeout=86400)
 def constituency(ons_code):
-    constituency = Constituency.query.get(ons_code)
+    constituency = Constituency.query.get_or_404(ons_code)
     return Response(repr(constituency), mimetype='application/json', status=200)
