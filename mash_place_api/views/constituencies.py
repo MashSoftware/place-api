3acb24f2670540cb1fdcb4082c60b78f11f7ce8e
matthew-shaw <matthew.shaw@digital.landregistry.gov.uk>
2016-10-21 16:28:24 +0100

    data catalogue route

--- mash_place_api/views/constituencies.py
+++ mash_place_api/views/constituencies.py
@@ -8,7 +8,7 @@ constituencies_bp = Blueprint('constituencies', __name__)
 
 @constituencies_bp.route('/', methods=['GET'])
 @cache.cached(timeout=86400)
-def constituencies():
+def get_constituencies():
     constituencies = Constituency.query.order_by(Constituency.name).all()
     results = []
     for constituency in constituencies:
@@ -17,8 +17,8 @@ def constituencies():
     return Response(json.dumps(results), mimetype='application/json', status=200)
 
 
-@constituencies_bp.route('/<ons_code>', methods=['GET'])
+@constituencies_bp.route('/<string:ons_code>', methods=['GET'])
 @cache.memoize(timeout=86400)
-def constituency(ons_code):
+def get_constituency(ons_code):
     constituency = Constituency.query.get_or_404(ons_code)
     return Response(constituency.get_geojson(), mimetype='application/json', status=200)
--- mash_place_api/views/general.py
+++ mash_place_api/views/general.py
@@ -1,11 +1,28 @@
-from flask import Response, Blueprint
+from flask import Response, Blueprint, request
 import json
 
 general_bp = Blueprint('general', __name__)
 
+routes = [{"url": "/constituencies",
+           "methods": ["GET"],
+           "description": "List of Westminster Constituencies",
+           "collection": "Boundaries"},
+          {"url": "/constituencies/<string:ons_code>",
+           "methods": ["GET"],
+           "description": "Westminster Constituency",
+           "collection": "Boundaries"}]
+
 
 @general_bp.route("/health", methods=['GET'])
-def check_status():
+def healthcheck():
     return Response(response=json.dumps({
         "status": "OK"
+    }), mimetype='application/json', status=200)
+
+
+@general_bp.route("/", methods=["GET"])
+def catalogue():
+    return Response(response=json.dumps({
+        "base_url": request.url_root[:-1],
+        "routes": routes
     }), mimetype='application/json', status=200)
